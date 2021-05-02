ca23cba3105e4a16131597159373b92929741160
Alan Iberra <alan@voyagerportal.com>
2021-05-02 18:41:15 -0300

    Strapi Quickstart

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f4d969d16f0bb465b4ea37769aa3c071'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8feafcf7-9716-4989-aa27-f2e07a16419d'
+};
